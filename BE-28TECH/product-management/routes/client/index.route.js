e399c7f9ddb396986dd3aacb328b6580f3418c85
lequangtuyen543 <lequangtuyen543@gmail.com>
2025-08-29 15:42:43 +0700

    fix

--- BE-28TECH/product-management/routes/client/index.route.js
+++
@@ -1,8 +0,0 @@
-const productsRoutes = require('./product.route');
-const homeRoutes = require('./home.route');
-
-module.exports = (app) => {  
-  app.use('/', homeRoutes);
-
-  app.use('/products', productsRoutes);
-}
