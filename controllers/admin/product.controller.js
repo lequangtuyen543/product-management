c6dde320e1cc745ab2c3ca01d0ad56aa3cfc0097
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-09-29 20:15:17 +0700

    4.1. Xóa vĩnh viễn

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -81,3 +81,12 @@ module.exports.changeMulti = async (req, res) => {
   }
   res.redirect(req.get("Referer") || "/");
 };
+
+//DELETE /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+  const id = req.params.id;
+
+  await Product.deleteOne({ _id: id });
+  // res.redirect("back");
+  res.redirect(req.get("Referer") || "/");
+};
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -1,4 +1,4 @@
-// changeStatus
+// Change Status
 const buttonsChangeStatus = document.querySelectorAll('[button-change-status]');
 if (buttonsChangeStatus.length > 0) {
   const formChangeStatus = document.querySelector('#form-change-status');
@@ -11,7 +11,7 @@ if (buttonsChangeStatus.length > 0) {
       const id = button.getAttribute('data-id');
 
       let statusChange = statusCurrent == 'active' ? 'inactive' : 'active';
-      
+
       const action = path + `/${statusChange}/${id}?_method=PATCH`;
       formChangeStatus.action = action;
 
@@ -19,3 +19,27 @@ if (buttonsChangeStatus.length > 0) {
     });
   });
 }
+// End Change Status
+
+//Delete Item
+const buttonsDelete = document.querySelectorAll('[button-delete]');
+if (buttonsDelete.length > 0) {
+  buttonsDelete.forEach((button) => {
+    const formDeleteItem = document.querySelector('#form-delete-item');
+    const path = formDeleteItem.getAttribute('data-path');
+
+    button.addEventListener('click', async (e) => {
+      const isConfirm = confirm('Are you sure to delete this item?');
+      if (isConfirm) {
+        const id = button.getAttribute('data-id');
+
+        const action = path + `/${id}?_method=DELETE`;
+
+        console.log(action);
+        formDeleteItem.action = action;
+        formDeleteItem.submit();
+      }
+    });
+  });
+}
+//End Delete Item
