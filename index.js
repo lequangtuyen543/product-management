2bbfc2d4749592cffbe7bdac263316331a0f81d8
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-10-20 16:29:20 +0700

    Config deploy online

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(methodOverride('_method'))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.set('views', './views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 
 // Flash
@@ -35,7 +35,7 @@ app.use(flash());
 // App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin
 
-app.use(express.static('public'));
+app.use(express.static(`${__dirname}/public`));
 
 //Routes
 route(app);
