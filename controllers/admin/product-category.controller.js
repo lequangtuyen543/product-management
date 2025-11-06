513dc76066845b59d6189822add28cf5ad32bc86
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-11-06 20:11:27 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Phần 1)

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,39 @@
+const ProductCategory = require('../../models/product-category.model');
+
+const systemConfig = require("../../config/system");
+
+//GET /admin/products-category
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await ProductCategory.find(find);
+
+  res.render('admin/pages/products-category/index', {
+    pageTitle: 'Page Category',
+    records: records,
+  });
+};
+
+//GET /admin/products-category/create
+module.exports.create = async (req, res) => {
+  res.render('admin/pages/products-category/create', {
+    pageTitle: 'Create Category',
+  });
+};
+
+//POST /admin/products-category/create
+module.exports.createPost = async (req, res) => {
+  if (req.body.position == "") {
+    const countCategories = await ProductCategory.countDocuments();
+    req.body.position = countCategories + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+
+  const record = new ProductCategory(req.body);
+  await record.save();
+
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,6 +2,7 @@ const systemConfig = require('../../config/system');
 
 const dashboardRoutes = require('./dashboard.route.js');
 const productRoutes = require('./product.route.js');
+const productCategoryRoutes = require('./product-category.route.js');
 
 module.exports = (app) => {  
   const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -9,4 +10,6 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + '/dashboard', dashboardRoutes);
   
   app.use(PATH_ADMIN + '/products', productRoutes);
+  
+  app.use(PATH_ADMIN + '/products-category', productCategoryRoutes);
 }
