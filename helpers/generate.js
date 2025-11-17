5829d9352543f9ef596ef0ae002927b8e1b84807
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-11-17 15:28:07 +0700

    1.1. Tạo collection accounts

---
+++ helpers/generate.js
@@ -0,0 +1,8 @@
+module.exports.generateRandomString = (length = 20) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return token;
+};
