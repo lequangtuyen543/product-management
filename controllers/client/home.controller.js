7990253085e9e654be15284d47312058b804b859
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-11-27 14:05:19 +0700

    4.1. Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,21 @@
+const Product = require('../../models/product.model');
+
+const productHelper = require('../../helpers/product');
+
 // [GET]
 module.exports.index = async (req, res) => {
+  //Lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: '1',
+    deleted: false,
+    status: 'active'
+  });
+
+  const newProducts = productHelper.priceNewProducts(productsFeatured);
+  //Lấy ra sản phẩm nổi bật
+
   res.render('client/pages/home/index', {
     pageTitle: 'Page Home',
+    productsFeatured: newProducts
   });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require('../../models/product.model');
 
+const productHelper = require('../../helpers/product');
+
 //[GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
@@ -7,10 +9,7 @@ module.exports.index = async (req, res) => {
     deleted: false
   }).sort({ position: 'desc' });
 
-  const newProducts = products.map((item) => {
-    item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
-    return item;
-  })
+  const newProducts = productHelper.priceNewProducts(products);
 
   res.render('client/pages/products/index', {
     pageTitle: 'Page Products',
