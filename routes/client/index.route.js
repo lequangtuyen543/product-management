630667916675f99e89ad7a858917fc1bdb72731e
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-11-30 22:20:32 +0700

    2.1. Thêm sản phẩm vào giỏ hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,15 +1,20 @@
 const categoryMiddleware = require('../../middlewares/client/category.middleware')
+const cartMiddleware = require('../../middlewares/client/cart.middleware')
 
 const homeRoutes = require('./home.route');
 const productsRoutes = require('./product.route');
 const searchRoutes = require('./search.route');
+const cartRoutes = require('./cart.route');
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
+  app.use(cartMiddleware.cartId);
 
   app.use('/', homeRoutes);
 
   app.use('/products', productsRoutes);
 
   app.use('/search', searchRoutes);
+
+  app.use('/cart', cartRoutes);
 }
