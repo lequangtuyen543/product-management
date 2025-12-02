e5a446411b55771224b8382df995d514b06282b3
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-02 18:00:56 +0700

    2.3. Làm trang giỏ hàng

--- helpers/product.js
+++ helpers/product.js
@@ -9,6 +9,6 @@ module.exports.priceNewProducts = (products) => {
 
 module.exports.priceNewProduct = (product) => {
   const priceNew = (product.price * (100 - product.discountPercentage) / 100).toFixed(0);
-  
-  return priceNew;
+
+  return parseInt(priceNew);
 }
