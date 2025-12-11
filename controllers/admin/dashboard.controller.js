4463c39b9a733b1e665b31298e956cbaca871f09
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-11 18:41:41 +0700

    4. Trang Dashboard bên admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,46 @@
+const Product = require("../../models/product.model");
+
 //[GET] /admin/dashboard
-module.exports.dashboard = (req, res) => {
+module.exports.dashboard = async (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+
+  statistic.product.total = await Product.countDocuments({
+    deleted: false
+  });
+
+  statistic.product.active = await Product.countDocuments({
+    deleted: false,
+    status: "active"
+  });
+
+  statistic.product.inactive = await Product.countDocuments({
+    deleted: false,
+    status: "inactive"
+  });
+
   res.render('admin/pages/dashboard/index', {
-    pageTitle: 'Page Dashboard'
+    pageTitle: 'Page Dashboard',
+    statistic: statistic
   });
 };
