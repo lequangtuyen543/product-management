00def9b9eef8d5488f433d97e398c84a233ce97b
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-18 14:15:57 +0700

    Tối ưu lại folder socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,50 +1,12 @@
 const Chat = require('../../models/chat.model')
 const User = require('../../models/user.model')
 
-const uploadToCloudinary = require('../../helpers/uploadToCloudinary')
+const chatSocket = require('../../sockets/client/chat.socket')
 
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-
   //Socket IO
-  _io.once('connection', (socket) => {
-    socket.on('CLIENT_SEND_MESSAGE', async (data) => {
-      let images = []
-      
-      for(const imageBuffer of data.images){
-        const link = await uploadToCloudinary(imageBuffer)
-        images.push(link)
-      }
-
-      //Lưu vào database
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        images: images
-      })
-      await chat.save()
-
-      //Trả data về client
-      _io.emit('SERVER_RETURN_MESSAGE', {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        images: images
-      })
-    })
-
-    // Typing
-    socket.on('CLIENT_SEND_TYPING', async (type) => {
-      socket.broadcast.emit('SERVER_RETURN_TYPING', {
-        userId: userId,
-        fullName: fullName,
-        type: type  
-      })
-    })
-    // End Typing
-  });
+  chatSocket(res)
   //End Socket IO
 
   // Lấy data từ database
