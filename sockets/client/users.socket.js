fb2cd2a24870d29cec4f77727e95ea8f3e34ab06
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-20 19:24:19 +0700

    1.6. Chức năng chấp nhận kết bạn

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -97,5 +97,52 @@ module.exports = (res) => {
         })
       }
     })
+
+    // Chức năng chấp nhận kết bạn
+    socket.on('CLIENT_ACCEPT_FRIEND', async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      // Thêm {user_id, room_chat_id} của A vào friendList của B
+      // Xóa id của A trong acceptFriends của B
+      const existIdAinB = await User.findOne({
+        _id: myUserId,
+        acceptFriends: userId
+      })
+
+      if (existIdAinB) {
+        await User.updateOne({
+          _id: myUserId,
+        }, {
+          $push: {
+            friendList: {
+              user_id: userId,
+              room_chat_id: "",
+            }
+          },
+          $pull: { acceptFriends: userId }
+        })
+      }
+
+      // Thêm {user_id, room_chat_id} của B vào friendList của A
+      // Xóa id của B trong requestFriends của A
+      const existIdBinA = await User.findOne({
+        _id: userId,
+        requestFriends: myUserId
+      })
+
+      if (existIdBinA) {
+        await User.updateOne({
+          _id: userId,
+        }, {
+          $push: {
+            friendList: {
+              user_id: myUserId,
+              room_chat_id: "",
+            }
+          },
+          $pull: { requestFriends: myUserId }
+        })
+      }
+    })
   });
 }
