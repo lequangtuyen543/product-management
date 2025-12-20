f72f2ac4a21682d588f75b5a9d7c93bd2f31d527
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-20 14:00:32 +0700

    1.5. Chức năng từ chối kết bạn

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -59,3 +59,29 @@ module.exports.request = async (req, res) => {
     users: users
   });
 }
+
+// [GET] /users/accept
+module.exports.accept = async (req, res) => {
+  // Socket
+  usersSocket(res)
+  // End Socket 
+
+  const userId = res.locals.user.id;
+
+  const myUser = await User.findOne({
+    _id: userId
+  })
+
+  const acceptFriends = myUser.acceptFriends;
+
+  const users = await User.find({
+    _id: { $in: acceptFriends },
+    status: 'active',
+    deleted: false
+  }).select('id avatar fullName')
+
+  res.render('client/pages/users/accept', {
+    pageTitle: 'Accept Friends',
+    users: users
+  });
+}
