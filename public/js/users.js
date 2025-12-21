f10b8e1fdbf9ad71cda614594189dd49dc183afb
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-21 19:51:50 +0700

    1.3. Hiển thị A vào danh sách Lời mời kết bạn của B

--- public/js/users.js
+++ public/js/users.js
@@ -27,29 +27,35 @@ if (listBtnCancelFriend.length > 0) {
 // Hết Chức năng hủy yêu cầu
 
 // Chức năng từ chối kết bạn
+const refuseFriend = (button) => {
+  button.addEventListener('click', () => {
+    button.closest('.box-user').classList.add('refuse')
+    const userId = button.getAttribute('btn-refuse-friend')
+
+    socket.emit('CLIENT_REFUSE_FRIEND', userId)
+  })
+}
 const listBtnRefuseFriend = document.querySelectorAll('[btn-refuse-friend]')
 if (listBtnRefuseFriend.length > 0) {
   listBtnRefuseFriend.forEach(button => {
-    button.addEventListener('click', () => {
-      button.closest('.box-user').classList.add('refuse')
-      const userId = button.getAttribute('btn-refuse-friend')
-
-      socket.emit('CLIENT_REFUSE_FRIEND', userId)
-    })
+    refuseFriend(button)
   })
 }
 // Hết Chức năng từ chối kết bạn
 
 // Chức năng chấp nhận kết bạn
+const acceptFriend = (button) => {
+  button.addEventListener('click', () => {
+    button.closest('.box-user').classList.add('accepted')
+    const userId = button.getAttribute('btn-accept-friend')
+
+    socket.emit('CLIENT_ACCEPT_FRIEND', userId)
+  })
+}
 const listBtnAcceptFriend = document.querySelectorAll('[btn-accept-friend]')
 if (listBtnAcceptFriend.length > 0) {
   listBtnAcceptFriend.forEach(button => {
-    button.addEventListener('click', () => {
-      button.closest('.box-user').classList.add('accepted')
-      const userId = button.getAttribute('btn-accept-friend')
-
-      socket.emit('CLIENT_ACCEPT_FRIEND', userId)
-    })
+    acceptFriend(button)
   })
 }
 // Hết Chức năng chấp nhận kết bạn
@@ -64,4 +70,50 @@ if (badgeUsersAccept) {
     }
   })
 }
-// End SERVER_RETURN_LENGTH_ACCEPT_FRIEND 
+// End SERVER_RETURN_LENGTH_ACCEPT_FRIEND
+
+// SERVER_RETURN_INFO_ACCEPT_FRIEND
+const dataUsersAccept = document.querySelector('[data-users-accept]');
+if (dataUsersAccept) {
+  const userId = dataUsersAccept.getAttribute('data-users-accept')
+  socket.on('SERVER_RETURN_INFO_ACCEPT_FRIEND', (data) => {
+    if (userId === data.userId) {
+      // Vẽ user ra giao diện      
+      const div = document.createElement("div");
+      div.classList.add("col-6");
+      div.setAttribute("user-id", data.infoUserA._id);
+
+      div.innerHTML = `
+        <div class="box-user">
+          <div class="inner-avatar">
+            <img src="https://robohash.org/hicveldicta.png" alt="${data.infoUserA.fullName}">
+          </div>
+          <div class="inner-info">
+            <div class="inner-name">${data.infoUserA.fullName}</div>
+            <div class="inner-buttons">
+              <button class="mr-1 btn btn-sm btn-primary" btn-accept-friend=${data.infoUserA._id}>Chấp nhận</button>
+              <button class="mr-1 btn btn-sm btn-secondary" btn-refuse-friend=${data.infoUserA._id}>Xóa</button>
+              <button class="mr-1 btn btn-sm btn-secondary" btn-deleted-friend="" disabled>Đã xóa</button>
+              <button class="mr-1 btn btn-sm btn-primary" btn-accepted-friend="" disabled>Đã chấp nhận</button>
+            </div>
+          </div>
+        </div>
+      `
+
+      dataUsersAccept.appendChild(div);
+      // Hết Vẽ user ra giao diện
+
+      // Hủy lời mời kết bạn 
+      const buttonRefuse = div.querySelector('[btn-refuse-friend]')
+
+      refuseFriend(buttonRefuse)
+      // End Hủy lời mời kết bạn 
+
+      // Chấp nhận lời mời kết bạn
+      const buttonAccept = div.querySelector('[btn-accept-friend]')
+      acceptFriend(buttonAccept)
+      // End Chấp nhận lời mời kết bạn
+    }
+  })
+}
+// End SERVER_RETURN_INFO_ACCEPT_FRIEND
--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -87,6 +87,16 @@ module.exports = (res) => {
         userId: userId,
         lengthAcceptFriends: lengthAcceptFriends
       })
+
+      // Lấy info của A trả về cho B
+      const infoUserA = await User.findOne({
+        _id: myUserId
+      }).select('id avatar fullName')
+
+      socket.broadcast.emit('SERVER_RETURN_INFO_ACCEPT_FRIEND', {
+        userId: userId,
+        infoUserA: infoUserA
+      })
     })
 
     // Chức năng từ chối kết bạn
