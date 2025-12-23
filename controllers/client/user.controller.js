dcec3185ad81bbcd2d6a3e2e13bba65dd7f58e5a
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-23 17:59:01 +0700

    2.2. Hiển thị realtime

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -84,6 +84,13 @@ module.exports.loginPost = async (req, res) => {
     statusOnline: 'online'
   })
 
+  _io.once('connection', (socket) => {
+    socket.broadcast.emit('SERVER_RETURN_USER_STATUS_ONLINE', {
+      userId: user.id,
+      status: 'online'
+    });
+  });
+
   res.redirect('/')
 }
 
@@ -94,6 +101,13 @@ module.exports.logout = async (req, res) => {
   }, {
     statusOnline: 'offline'
   })
+
+  _io.once('connection', (socket) => {
+    socket.broadcast.emit('SERVER_RETURN_USER_STATUS_ONLINE', {
+      userId: res.locals.user.id,
+      status: 'offline'
+    });
+  });
 
   res.clearCookie('tokenUser')
   res.clearCookie('cartId')
